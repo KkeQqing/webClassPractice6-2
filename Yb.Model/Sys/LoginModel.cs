da088cc7710999b92cccaf00efbd44e357b61737
kq <1798178975@qq.com>
2025-11-22 15:41:11 +0800

    添加JWT配置和LoginModel模型

--- Yb.Model/Sys/LoginModel.cs
+++ Yb.Model/Sys/LoginModel.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Yb.Model.Sys
+﻿namespace Yb.Model.Sys
 {
-    internal class LoginModel
+    public class LoginModel
     {
+        public string Account { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
