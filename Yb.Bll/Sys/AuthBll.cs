930a5adb9908123f6824a5d2f2e4c649a19d04ef
kq <1798178975@qq.com>
2025-11-22 15:18:05 +0800

    添加 AuthBll 登录验证业务逻辑

---
+++ Yb.Bll/Sys/AuthBll.cs
@@ -0,0 +1,22 @@
+﻿using Yb.Dal.Sys;
+using Yb.Model.Sys;
+using Yb.Utility.Security;
+
+namespace Yb.Bll.Sys
+{
+    public class AuthBll
+    {
+        private readonly YbUserDal _userDal;
+
+        public AuthBll(YbUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public YbUser? GetLoginUser(LoginModel loginModel)
+        {
+            loginModel.Password = SecurityUtility.BuildPassword(loginModel.Password);
+            return _userDal.Query(o => o.Account == loginModel.Account && o.Password == loginModel.Password).FirstOrDefault();
+        }
+    }
+}
