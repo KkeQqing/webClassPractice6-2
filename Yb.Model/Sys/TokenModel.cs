42c9a3998da3acfd43a7b587b6777c4e423393ad
kq <1798178975@qq.com>
2025-11-22 15:16:30 +0800

    新增登录和 Token 返回模型（LoginModel / TokenModel）

---
+++ Yb.Model/Sys/TokenModel.cs
@@ -0,0 +1,48 @@
+﻿namespace Yb.Model.Sys
+{
+    public class TokenModel
+    {
+        public string Id { get; set; }
+        public string Account { get; set; }
+        public string UserNM { get; set; }
+        public string UserCD { get; set; }
+        public string? DepartmentCD { get; set; }
+        public string? DepartmentNM { get; set; }
+        public string RoleID { get; set; }
+        public string RoleName { get; set; }
+        public string? Photo { get; set; }
+        public int DataAuthority { get; set; }
+        public DateTime TokenTime { get; set; } = DateTime.Now;
+        public string? Token { get; set; }
+        public string? TokenType { get; set; } = "Bearer";
+
+        public Profile? profile;
+
+        public TokenModel() { }
+
+        public TokenModel(YbUser ybUser)
+        {
+            Id = ybUser.Id;
+            Account = ybUser.Account;
+            UserNM = ybUser.UserNM;
+            UserCD = ybUser.UserCD;
+            DepartmentCD = ybUser.DepartmentCD;
+            DepartmentNM = ybUser.DepartmentNM;
+            RoleID = ybUser.RoleID;
+            RoleName = ybUser.RoleName;
+            Photo = ybUser.Photo;
+            DataAuthority = ybUser.DataAuthority;
+            Profile profile = new Profile();
+            profile.sid = ybUser.Id;
+            profile.name = ybUser.UserNM;
+        }
+    }
+
+    public class Profile
+    {
+        public string? sid { get; set; }
+        public string? name { get; set; }
+        public DateTime? auth_time { get; set; } = DateTime.Now;
+        public DateTime? expires_at { get; set; } = DateTime.Now;
+    }
+}
