c5ee908c1783a5bdcb50671a9ed923715f47c96e
kq <1798178975@qq.com>
2025-11-22 15:30:12 +0800

    添加api.controllers.base引用

--- Yb.Api/Controllers/Sys/AuthController.cs
+++ Yb.Api/Controllers/Sys/AuthController.cs
@@ -1,6 +1,41 @@
-﻿namespace Yb.Api.Controllers.Sys
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using Yb.Bll.Sys;
+using Yb.Model.Sys;
+using Yb.Api.Controllers.Base;
+
+namespace Yb.Api.Controllers.Sys
 {
-    public class AuthController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
     {
+        private readonly AuthBll _authBll;
+
+        public AuthController(AuthBll authBll)
+        {
+            _authBll = authBll;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel loginModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new { message = "Invalid input" });
+
+            var user = _authBll.GetLoginUser(loginModel);
+            if (user == null)
+                return Unauthorized(new { message = "Invalid account or password" });
+
+            var tokenModel = new TokenModel(user);
+            tokenModel.Token = JwtHelper.IssueJWT(tokenModel);
+
+            return Ok(new
+            {
+                success = true,
+                data = tokenModel,
+                message = "Login successful"
+            });
+        }
     }
 }
