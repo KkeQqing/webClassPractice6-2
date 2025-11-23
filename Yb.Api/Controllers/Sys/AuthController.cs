2ac38862cdb99957325904f6ea63249f886a6c10
kq <1798178975@qq.com>
2025-11-23 22:16:32 +0800

    注入 IMemoryCache 并实现验证码接口

--- Yb.Api/Controllers/Sys/AuthController.cs
+++ Yb.Api/Controllers/Sys/AuthController.cs
@@ -4,6 +4,8 @@ using System.IdentityModel.Tokens.Jwt;
 using Yb.Api.Controllers.Base;
 using Yb.Bll.Sys;
 using Yb.Model.Sys;
+using Microsoft.Extensions.Caching.Memory;
+using Yb.Utility.Security;
 
 namespace Yb.Api.Controllers.Sys
 {
@@ -12,10 +14,13 @@ namespace Yb.Api.Controllers.Sys
     public class AuthController : ControllerBase
     {
         private readonly AuthBll _authBll;
+        private readonly IMemoryCache _memoryCache;
 
-        public AuthController(AuthBll authBll)
+        // 同时注入 AuthBll 和 IMemoryCache
+        public AuthController(AuthBll authBll, IMemoryCache memoryCache)
         {
             _authBll = authBll;
+            _memoryCache = memoryCache;
         }
 
         [HttpPost("login")]
@@ -36,6 +41,30 @@ namespace Yb.Api.Controllers.Sys
             {
                 Msg = "Login successful"
             });
+        }
+
+        /// <summary>
+        /// 获取验证码图片
+        /// </summary>
+        /// <param name="key">缓存 key，默认为 k1</param>
+        /// <returns>图片流</returns>
+        [HttpGet("captcha")]
+        [AllowAnonymous]
+        public FileContentResult GetCaptchaCode([FromQuery] string key = "k1")
+        {
+            var captchaHelper = new CaptchaHelper();
+            string code = captchaHelper.CreateValidateCode(4); // 4位验证码
+            byte[] imageData = captchaHelper.CreateValidateGraphic(code);
+
+            // 将验证码存入内存缓存，10分钟后过期
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10)
+            };
+
+            _memoryCache.Set(key, code, cacheEntryOptions);
+
+            return File(imageData, "image/jpeg");
         }
     }
 }
